ffd121e6f65c647fc33d8034b37aa4f85b41f919
Tohew <baophong2401@gmail.com>
2024-03-24 22:10:11 +0700

    soft delete

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,9 +4,21 @@ const Course = require('../models/Course');
 class MeController {
     // GET /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then((courses) =>
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deleteCount]) =>
                 res.render('me/store-courses', {
+                    deleteCount,
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next);
+    }
+
+    // GET /me/trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({})
+            .then((courses) =>
+                res.render('me/trash-courses', {
                     courses: multipleMongooseToObject(courses),
                 }),
             )
